1454b1e54640f3e5d9b8b46e03c5603c9f655b72
LabUser <LabUser@localhost>
2026-01-12 14:02:37 -0800

    Working on gateway and dns and ip address config.

--- configuration.ps1
+++ configuration.ps1
@@ -5,12 +5,34 @@ configuration ContosoWebServer {
         )
 
             Import-DscResource -ModuleName PSDesiredStateConfiguration
+            Import-DscResource -ModuleName NetworkingDsc
                 # Add other modules like xWebAdministration if needed
 
                     Node $NodeName {
                         # Install IIS
                         WindowsFeature IIS {Name='Web-Server'; Ensure='Present'}
 
+                        # Configure Network Adapter
+                        IPAddress SetIPAddress {
+                            InterfaceAlias ='Ethernet'
+                            Addressfamily = 'IPv4'
+                            IPAddress = 192.168.1.2/24
+                            KeepExistingAddress = $False
+                        }
+
+                        DefaultGatewayAddress setGateway {
+                            InterfaceAlias =  'Ethernet'
+                            AddressFamily =  'IPv4'
+                            Address = 192.168.1.10
+                            DependsOn = '[IPAddress]SetIpAddress'
+                        }
+
+                        DnsServerAddress setDns {
+                            InterfaceAlias = 'Ethernet'
+                            AddressFamily =  'IPv4'
+                            Address = 192.168.1.1
+                            DependsOn = '[IPAddress]SetIPAddress'
+                        }
                     }
                 }
 
