3fa91867985e736ce02c3c488fa2669b6b26cefb
LabUser <LabUser@localhost>
2026-01-13 15:30:26 -0800

    Initial files provided for lab 6

---
+++ PShell/Labs/configuration.ps1
@@ -0,0 +1,107 @@
+﻿$configData = Import-LocalizedData -BaseDirectory C:\PShell\Labs -FileName configurationdata.psd1 -SupportedCommand New-Object,ConvertTo-SecureString
+
+configuration WebServer
+{
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName WebAdministrationDsc
+    Import-DscResource -ModuleName NetworkingDsc
+
+    node $AllNodes.Where( { $_.Role -eq 'WebServer' }).NodeName
+    {
+        $nodeConfig = $configurationData[$Node.Role]
+
+        # Copy content
+        $dependencies = @()
+        foreach ($folder in $nodeConfig.Folders)
+        {
+            $resourceName = $folder.Source -replace '\W'
+            $dependencies += "[File]$resourceName"
+            File $resourceName
+            {
+                SourcePath      = $folder.Source
+                DestinationPath = $folder.Destination
+                Type            = 'Directory'
+                Ensure          = 'Present'
+            }
+        }
+
+        # Install features
+        foreach ($feature in $nodeConfig.Features)
+        {
+            $dependencies += "[WindowsFeature]$feature"
+            WindowsFeature $feature
+            {
+                Name   = $feature
+                Ensure = 'Present'
+            }
+        }
+
+        foreach ($interface in $node.Interfaces)
+        {
+            NetIPInterface "DisableDhcp_$($interface.InterfaceAlias)"
+            {
+                InterfaceAlias = $interface.InterfaceAlias
+                AddressFamily  = 'IPv4'
+                Dhcp           = 'Disabled'
+            }
+
+            IPAddress "NetworkIp_$($interface.InterfaceAlias)"
+            {
+                IPAddress      = $interface.IpAddress
+                AddressFamily  = 'IPv4'
+                InterfaceAlias = $interface.InterfaceAlias
+            }
+
+            DefaultGatewayAddress "DefaultGateway_$($interface.InterfaceAlias)"
+            {
+                AddressFamily  = 'IPv4'
+                InterfaceAlias = $interface.InterfaceAlias
+                Address        = $interface.Gateway
+            }
+        }
+
+        # Create web sites
+        foreach ($site in $nodeConfig.WebSites)
+        {
+            WebAppPool $site.AppPool.Name
+            {
+                Name = $site.AppPool.Name
+                Credential = $site.AppPool.Credential
+            }
+            WebSite $site.Name
+            {
+                Name            = $site.Name
+                PhysicalPath    = $site.PhysicalPath
+                ApplicationPool = $($site.AppPool.Name)
+                BindingInfo = DSC_WebBindingInformation {
+                    Port = $site.Port
+                    Protocol =  $site.Protocol
+                }
+                DependsOn       = $dependencies, "[WebAppPool]$($site.AppPool.Name)"
+            }
+        }
+    }
+}
+
+WebServer -ConfigurationData $configData
+
+[DscLocalConfigurationManager()]
+configuration LCMConfig
+{
+    node $AllNodes.Where({$_.Role -eq 'WebServer'}).NodeName
+    {
+        Settings
+        {
+            # Default is Push - Possible: Disabled, Push, Pull
+            RefreshMode = $node.RefreshMode
+            # Default is ApplyAndMonitor - Possible: MonitorOnly,ApplyOnly,ApplyAndMonitor or ApplyAndAutoCorrect
+            ConfigurationMode =  $Node.ConfigurationMode
+            # This is the thumbprint of the certificate to be used to decrypt credentials in the MOF
+            CertificateId = $node.CertificateThumbprint
+            # Default is False - This will allow the LCM to reboot the node it a resource requires it.
+            RebootNodeIfNeeded = $node.RebootNodeIfNeeded  
+        }
+    }
+}
+
+LcmConfig -COnfigurationData $configData
---
+++ PShell/Labs/configurationdata.psd1
@@ -0,0 +1,57 @@
+﻿@{
+    AllNodes  = @(
+        @{
+            NodeName              = 'MS'
+            Role                  = 'WebServer'
+            RefreshMode           = 'Push'
+            ConfigurationMode     = 'ApplyAndAutoCorrect'
+            CertificateThumbprint = '71C27CECB4194E821C7A4B008F77DCD9046B8C39'
+            RebootNodeIfNeeded    = $true
+            CertificateFile       = 'C:\PShell\Labs\DscPublicKey.cer'
+            Interfaces            = @(
+                @{
+                    'InterfaceAlias' = 'Ethernet'
+                    'IpAddress'      = '192.168.1.2/24'
+                    'Gateway'        = '192.168.1.10'
+                    'DnsServer'      = '192.168.1.1'
+                }
+            )
+        }
+    )
+
+    WebServer = @{
+        Features = 'Web-Server'
+        Folders  = @(
+            @{
+                Source      = '\\Client01\PShell\Site1'
+                Destination = 'C:\Site1'
+            }
+            @{
+                Source      = '\\Client01\PShell\Site2'
+                Destination = 'C:\Site2'
+            }
+        )
+        WebSites = @(
+            @{
+                Name         = 'Site1'
+                PhysicalPath = 'C:\Site1'
+                Port         = 8080
+                Protocol     = 'http'
+                AppPool      = @{
+                    Name       = 'CustomAppPool1'           
+                    Credential = New-Object -TypeName PSCredential -ArgumentList 'AppPoolUser', (ConvertTo-SecureString -String 'Pa$$w0rd' -AsPlainText -Force)
+                }            
+            }            
+            @{                
+                Name         = 'Site2'
+                PhysicalPath = 'C:\Site2'
+                Port         = 8081
+                Protocol     = 'http'
+                AppPool      = @{
+                    Name       = 'CustomAppPool2'
+                    Credential = New-Object -TypeName PSCredential -ArgumentList 'AppPoolUser', (ConvertTo-SecureString -String 'Pa$$w0rd' -AsPlainText -Force)
+                }
+            }
+        )
+    }
+}
